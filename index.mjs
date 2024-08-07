7f5ed16e5b4b4c3bc07ba08536b808624176ef15
BlaNKtext <blanktextt@gmail.com>
2024-08-07 14:35:51 -0400

    d

---
+++ index.mjs
@@ -0,0 +1,28 @@
+import { AutoRouter, cors } from 'itty-router'
+import { createServerAdapter } from '@whatwg-node/server'
+import { createServer } from 'http'
+import url  from 'url'
+const { preflight, corsify } = cors() 
+const router = AutoRouter({
+  before: [preflight],
+  finally: [corsify]
+})
+const playString = []
+router.get("/", () => {
+  return new Response('Whoops! Are you sure you know what youre doing?')
+})
+router.get("/get",(req, res) => {
+    const returnData = JSON.stringify(playString)
+    return new Response(returnData)
+  })
+router.get("/post",(req, res) => {
+    let q = url.parse(req.url, true).query
+    if (q.title || q.sid) {playString.push(q)}
+    if (playString.length > 16) {playString.shift()}
+    return new Response("")
+  })
+router.all("*", () => new Response("This route doesnt exist, silly!", {status: 404}))
+const ittyServer = createServerAdapter(router.fetch)
+const httpServer = createServer(ittyServer)
+httpServer.listen(3001)
+export default router
